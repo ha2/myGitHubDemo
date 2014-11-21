fbd2dbd6ced639033b52210740b4135829bf007f
ha2 <hegedusaron2@gmail.com>
2014-11-21 21:30:10 +0100

    2014-11-21

--- app/src/main/java/hu/app/android/demo/githubdemo/MainActivity.java
+++ app/src/main/java/hu/app/android/demo/githubdemo/MainActivity.java
@@ -1,9 +1,17 @@
 package hu.app.android.demo.githubdemo;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends Activity {
@@ -15,25 +23,5 @@ public class MainActivity extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
